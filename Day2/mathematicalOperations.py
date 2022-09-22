a25fcc6e26391095b36f37105788175d12ee80a0
Deeplaxmi Lambture <lambture.d.p.10@gamil.com>
2022-09-22 06:21:23 +0530

    mathematical operations

---
+++ Day2/mathematicalOperations.py
@@ -0,0 +1,20 @@
+#Addition
+print(3+5)
+#Substraction
+print(7-4)
+#multiplication
+print(2*3)
+#division
+print(4/2)  #Always gives float type of answer
+#power of exponent
+print(2 ** 2)
+
+
+
+#priority - PEMDAS  -> calculation goes from left to right
+# Parenthesis ()
+# Exponents **
+# Multiplication * or  Division /
+# Addition + or Substraction -
+
+print(3 * 3 + 3 / 3 - 3)
