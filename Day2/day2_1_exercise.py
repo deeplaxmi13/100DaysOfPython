2068b3b9bc4cd896f9258bb94002d65635f57524
Deeplaxmi Lambture <lambture.d.p.10@gamil.com>
2022-09-22 06:09:49 +0530

    exercise 2_1_

---
+++ Day2/day2_1_exercise.py
@@ -0,0 +1,6 @@
+two_digit_number = input("Enter a two digit number")
+print(type(two_digit_number))
+
+sum =int(two_digit_number[0]) + int(two_digit_number[1])
+print("the sum of two digit number is "+ str(sum))
+
