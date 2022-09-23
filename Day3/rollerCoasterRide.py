17c6113138b5046b57484437f121dae0e21ac13c
Deeplaxmi Lambture <lambture.d.p.10@gamil.com>
2022-09-23 19:43:23 +0530

    roller coaster example

---
+++ Day3/rollerCoasterRide.py
@@ -0,0 +1,8 @@
+print("Welcome to the Roller Coaster")
+
+height = float(input("Enter you height in cm: "))
+
+if height > 150:
+    print("You can ride the roller coaster")
+else:
+    print("You can't ride the roller coaster")
