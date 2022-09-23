4a630d064af778d3a29f846c8f12516f9cc46a28
Deeplaxmi Lambture <lambture.d.p.10@gamil.com>
2022-09-23 18:56:40 +0530

    Number Manupilation and F string

---
+++ Day2/NumberManipulationAndFStrings.py
@@ -0,0 +1,18 @@
+print(round(10/3)) # gives the round answer whichever is near
+
+print(round(8/3))
+
+print(8//3)  #floor division - removes the decimal point and prints an integer
+
+
+result = 4/2
+result /= 2
+print("result "+ str(result))
+
+#Fstrings
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height} and you are winning is {isWinning}")
+
