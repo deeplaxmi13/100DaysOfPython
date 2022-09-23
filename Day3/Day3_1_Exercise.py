9226bfa1b5f8cc28dbe107e0ae8bca42916b7cd8
Deeplaxmi Lambture <lambture.d.p.10@gamil.com>
2022-09-23 19:49:15 +0530

    Exercise 1 of Day 3

---
+++ Day3/Day3_1_Exercise.py
@@ -0,0 +1,7 @@
+#write a program that check if a given number is odd or even
+
+number = int(input("Enter a number: "))
+if number%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
