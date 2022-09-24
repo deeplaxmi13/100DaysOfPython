e004af0e512e72f7609c898ecbeace7cbf7b2e93
deeplaxmi lambture <lambture.d.p.10@gmail.com>
2022-09-24 05:54:02 +0530

    BMI 2.0

--- Day3/BMI_2.0.py
+++ Day3/BMI_2.0.py
@@ -6,8 +6,6 @@ height = float(input("Enter your height in cm: "))
 
 BMIIndex = int(weight/(height ** 2))
 
-
-
 if BMIIndex < 18.5:
     print(f"Your BMI value is {BMIIndex} and indicates that you are underweight")
 elif BMIIndex >=18.5 and BMIIndex < 25:
