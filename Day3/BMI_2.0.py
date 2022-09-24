d2a16e27048e8a30ee35445bca0408e9ec93801e
deeplaxmi lambture <lambture.d.p.10@gmail.com>
2022-09-24 06:03:52 +0530

    BMI 2.0

--- Day3/BMI_2.0.py
+++ Day3/BMI_2.0.py
@@ -4,13 +4,15 @@ weight = float(input("Enter your weight in kg: "))
 
 height = float(input("Enter your height in cm: "))
 
-BMIIndex = int(weight/(height ** 2))
+BMIIndex = round(weight/(height ** 2))
 
 if BMIIndex < 18.5:
     print(f"Your BMI value is {BMIIndex} and indicates that you are underweight")
-elif BMIIndex >=18.5 and BMIIndex < 25:
+elif BMIIndex < 25:
     print(f"Your BMI value is {BMIIndex} and indicates that you have a normal weight")
-elif BMIIndex >=25 and BMIIndex < 30:
+elif BMIIndex < 30:
     print(f"Your BMI value is {BMIIndex} and indicates that you are overweight")
-else:
+elif BMIIndex < 35:
     print(f"Your BMI value is {BMIIndex} and indicates that you are obese")
+else:
+    print(f"Your BMI value is {BMIIndex} and you are clinically obese")
