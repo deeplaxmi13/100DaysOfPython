62b1eda078e431e989eb1d4032957904e18f1e03
Deeplaxmi Lambture <lambture.d.p.10@gamil.com>
2022-09-24 05:46:34 +0530

    BMI CALCULATOR 2.0

---
+++ Day3/BMI_2.0.py
@@ -0,0 +1,18 @@
+print("Welcome to BMI Calculator Version 2")
+
+weight = float(input("Enter your weight in kg: "))
+
+height = float(input("Enter your height in cm: "))
+
+BMIIndex = int(weight/(height ** 2))
+
+
+
+if BMIIndex < 18.5:
+    print(f"Your BMI value is {BMIIndex} and indicates that you are underweight")
+elif BMIIndex >=18.5 and BMIIndex < 25:
+    print(f"Your BMI value is {BMIIndex} and indicates that you have a normal weight")
+elif BMIIndex >=25 and BMIIndex < 30:
+    print(f"Your BMI value is {BMIIndex} and indicates that you are overweight")
+else:
+    print(f"Your BMI value is {BMIIndex} and indicates that you are obese")
