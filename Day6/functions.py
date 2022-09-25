e61de6cbbfb8a58aea4b6ece1b78eb1892c94720
deeplaxmi lambture <lambture.d.p.10@gmail.com>
2022-09-25 22:02:07 +0530

    Day 6

---
+++ Day6/functions.py
@@ -0,0 +1,20 @@
+def my_function():
+    print("Hello")
+
+
+'''
+def nameOfFunction():
+    do this
+    then do this
+    finally do this
+
+call the functions
+'''
+
+my_function()
+
+
+'''
+reeborg's world hurdle challenges using while & for & functions
+
+'''
